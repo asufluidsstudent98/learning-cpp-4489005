386e2ef8a9641c07cc52fdb17b95f8e6a97a2658
asufluidsstudent98 <rcluever@asu.edu>
2025-07-14 21:00:59 +0000

    worke

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,21 @@
 #include <cstdint>
 
 int main(){
+    int far = 100;
+    int celsius;
+
+    celsius = (float(5)/9.0)*(far-32);
     
+    std::cout << std::endl;
+    std::cout << "Fahrenheit: " << far << std::endl;
+    std::cout << "Celsius   : " << celsius << std::endl;
+
+    float weight = 10.99;
+    std::cout << std::endl;
+    std::cout << "Float        : " << weight << std::endl;
+    std::cout << "Integer      : " << (int) weight << std::endl;
+    std::cout << "Fractional   : " << (int)((weight - (int) weight)*10000) << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
