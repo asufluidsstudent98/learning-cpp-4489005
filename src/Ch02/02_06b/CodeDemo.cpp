48b3c6cd18adaf77e564e704fea82992bc870527
asufluidsstudent98 <rcluever@asu.edu>
2025-07-14 20:28:15 +0000

    stuff

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000 //if it finds CAPACITY it will replace value with 5000
+#define DEBUG 
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perfrom addtion]" << std::endl;
+#endif
+    large += small; //large = large + small;
+    std::cout << "the larger integer is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
