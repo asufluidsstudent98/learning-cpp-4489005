52bf960db84c1ae3132c9c97f10843114ba036da
asufluidsstudent98 <rcluever@asu.edu>
2025-07-14 20:07:39 +0000

    more

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -19,6 +19,12 @@ int main(){
     std::cout << " b = " << b << std::endl;
     std::cout << " flag = " << my_flag << std::endl;
 
+    my_flag = true;
+    std::cout << " flag = " << my_flag << std::endl;
+    std::cout << " a + b = " << a+b << std::endl;
+    std::cout << " b - a = " << b-a << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
