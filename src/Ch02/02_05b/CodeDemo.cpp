f634fedf289eda11b062b7117a53b6d3d46020a1
asufluidsstudent98 <rcluever@asu.edu>
2025-07-14 20:18:53 +0000

    work

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    
+    auto a = 8; //int
+    auto b = 12345678901; //long
+    auto c = 3.14f; //float
+    auto d  = 3.14; // double
+    auto e = true; // bool
+    auto f = 'd'; // char
+    auto g = "C++ sucks!"; //pointer construct char (string)
+
+    std::cout << "the type of a is " << typeid(a).name() << std::endl;
+    std::cout << "the type of b is " << typeid(b).name() << std::endl;
+    std::cout << "the type of c is " << typeid(c).name() << std::endl;
+    std::cout << "the type of d is " << typeid(d).name() << std::endl;
+    std::cout << "the type of e is " << typeid(e).name() << std::endl;
+    std::cout << "the type of f is " << typeid(f).name() << std::endl;
+    std::cout << "the type of g is " << typeid(g).name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
